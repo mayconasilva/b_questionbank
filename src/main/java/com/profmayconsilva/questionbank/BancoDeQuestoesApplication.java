1b0911e7270f86e6d88245c372e714c120f30cb3
Maycon Silva <73001893+mayconasilva@users.noreply.github.com>
2024-05-24 20:02:10 -0300

    First Commit

---
+++ src/main/java/com/profmayconsilva/questionbank/BancoDeQuestoesApplication.java
@@ -0,0 +1,13 @@
+package com.profmayconsilva.questionbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDeQuestoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDeQuestoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profmayconsilva/questionbank/BancoDeQuestoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profmayconsilva.questionbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeQuestoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
