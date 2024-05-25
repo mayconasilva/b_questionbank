69b4415f0b9848a68526abe58b09a60533f8614a
Maycon Silva <73001893+mayconasilva@users.noreply.github.com>
2024-05-25 15:14:50 -0300

    Added Exception Handler

--- src/main/java/com/profmayconsilva/questionbank/core/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/profmayconsilva/questionbank/core/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,6 +12,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidDataException.class)
     public ResponseEntity<String> handleInvalidDataException(InvalidDataException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<String> handleNoResourceException(NoResourceFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found. Check the requested URL.");
     }
 
     @ExceptionHandler(Exception.class)
